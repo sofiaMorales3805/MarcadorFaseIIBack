671d03f598b0c8c7025fcd559dc377df5bade19f
Alejandro <cristianmelgar1010@gmail.com>
2025-09-28 20:45:09 -0600

    Gestión de partidos controladores

---
+++ Models/Equipo.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using MarcadorFaseIIApi.Services.Interfaces;
+namespace MarcadorFaseIIApi.Models
+{
+    public class Equipo
+    {
+        public int Id { get; set; }
+        [Required]                 
+        public string Nombre { get; set; } = string.Empty;
+        public int Puntos { get; set; }
+        public int Faltas { get; set; }
+        public string Ciudad { get; set; } = string.Empty; 
+        public string? LogoFileName { get; set; }
+        public List<Jugador>? Jugadores { get; set; }
+    }
+}
